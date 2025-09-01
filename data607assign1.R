aeafc77a89e5a66711af332b2165cd280b323f36
WendyR20 <152345540+WendyR20@users.noreply.github.com>
2025-08-31 20:23:24 -0400

    Add files via upload

---
+++ data607assign1.R
@@ -0,0 +1,22 @@
+---
+  title: "Data 607 Assignment 1"
+author: "Wendy Romero"
+output:
+  pdf_document: default
+html_document:
+  includes:
+highlight: pygments
+theme: cerulean
+toc: true
+toc_float: true
+editor_options: 
+  chunk_output_type: console
+---
+  ```{r global_options, include=FALSE}
+knitr::opts_chunk$set(eval = TRUE, message = FALSE, warning = FALSE)
+library(tidyverse)
+```
+
+## Introduction
+
+## Conclusion
